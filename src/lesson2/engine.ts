e9b4698ad84b4b28474da02a2dc9c0fb616a2434
Corvax <vlvete1990@yandex.ru>
2020-05-10 19:42:49 +0300

    adding parser

--- src/lesson2/engine.ts
+++ src/lesson2/engine.ts
@@ -1,34 +1,34 @@
 import { ParsedLineType } from "./parser";
 import { isNumber } from "./helpers";
 import {
-  mathOperators,
-  mathPriorities,
-  mathOperatorsPriorities,
+    mathOperators,
+    mathPriorities,
+    mathOperatorsPriorities,
 } from "./mathOperators";
 
 const [FIRST, SECOND] = mathPriorities;
 
 export const firstPrioritiesCalc = (stack: ParsedLineType): ParsedLineType =>
-  stack.reduce<ParsedLineType>((result, nextItem) => {
+    stack.reduce<ParsedLineType>((result, nextItem) => {
     const prevItem = result[result.length - 2];
     const item = result[result.length - 1];
 
     if (!isNumber(String(item)) && mathOperatorsPriorities[item] === FIRST) {
-      if (!mathOperators[item]) {
-        throw new TypeError("Unexpected stack!");
-      }
-      result = [
-        ...result.slice(0, -2),
-        mathOperators[item](Number(prevItem), Number(nextItem)),
-      ];
+        if (!mathOperators[item]) {
+            throw new TypeError("Unexpected stack!");
+        }
+        result = [
+            ...result.slice(0, -2),
+            mathOperators[item](Number(prevItem), Number(nextItem)),
+        ];
     } else {
-      result.push(nextItem);
+        result.push(nextItem);
     }
     return result;
-  }, []);
+}, []);
 
 export const secondPrioritiesCalc = (stack: ParsedLineType): number =>
-  stack.reduce<number>((result, nextItem, key) => {
+stack.reduce<number>((result, nextItem, key) => {
     const item = stack[key - 1];
 
     if (mathOperatorsPriorities[item] === FIRST) {
@@ -39,4 +39,4 @@ export const secondPrioritiesCalc = (stack: ParsedLineType): number =>
       result = mathOperators[item](Number(result), Number(nextItem));
     }
     return result;
-  }, Number(stack[0]));
+}, Number(stack[0]));
