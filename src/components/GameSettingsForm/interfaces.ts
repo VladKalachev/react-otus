df44b9a33a76278ca744aaaf9b11dfc83a9251f4
Corvax <vlvete1990@yandex.ru>
2020-05-12 16:31:23 +0300

    addin lesson 8

--- src/components/GameSettingsForm/interfaces.ts
+++ src/components/GameSettingsForm/interfaces.ts
@@ -1,14 +1,14 @@
 export interface PlayerSettings {
-    name: string;
-    symbol: string;
-    color: string;
-  }
-  
-  export interface GameSettingsFormResult {
-    player1: PlayerSettings;
-    player2: PlayerSettings;
-  }
-  
-  export interface GameSettingsFormProps {
-    onSubmit: (settings: GameSettingsFormResult) => void;
-  }
+  name: string;
+  symbol: string;
+  color: string;
+}
+
+export interface GameSettingsFormResult {
+  player1: PlayerSettings;
+  player2: PlayerSettings;
+}
+
+export interface GameSettingsFormProps {
+  onSubmit: (settings: GameSettingsFormResult) => void;
+}
